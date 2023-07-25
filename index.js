2c0184749949690493ae6a2d6eb2767a19238045
Trysten M <nick.tristen22@gmail.com>
2023-07-25 18:24:59 +0300

    fix dir try 16

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ import cors from 'cors'
 import routes from "./routes/index.js";
 import {config} from "./config/index.js";
 import * as url from 'url';
+import * as path from "path";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 dotenv.config()
 
 const PORT = process.env.PORT || '5000'
 const API = Express()
-const path = __dirname + './views'
+// const path = __dirname + './views/dist'
+const filePath = path.join(__dirname, 'views/dist')
 
 API
     .use(bodyParser.json({limit: '20mb'}))
     .use(cors())
     .use(config.API_ROUTE, routes)
-    .use(Express.static(path))
+    .use(Express.static(filePath))
 
 const start = async () => {
     try {
@@ -30,7 +32,7 @@ const start = async () => {
         })
 
         API.get('/', (req, res) => {
-            res.sendFile(path + '/index.html')
+            res.sendFile(path.join(__dirname, 'views/dist/index.html'))
         })
     } catch (e) {
         console.log(e)
